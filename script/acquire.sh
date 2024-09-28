69af5cedf77619061a59e7760693ed36f4c5d414
haok1402 <haok1402@gmail.com>
2024-09-28 00:00:07 -0400

    clean up installation and scripts for acquiring a new compute node

--- script/acquire.sh
+++ script/acquire.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
-# Acquire a compute node with 32 CPUs, 96GB memory, and 1 A6000 GPU.
+# Acquire a compute node with 32 CPUs, 96GB RAM, and 1 A6000 GPU.
+# The node will be acquired for 7 days, and the session will be interactive.
 # Please run this script with tmux to avoid losing the session.
 srun \
     --partition=long --time=07-00:00:00 \
