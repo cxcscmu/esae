e0d9c9faa3cafcf5c5ac4f159b22046d72635e05
haok1402 <haok1402@gmail.com>
2024-10-04 21:53:29 -0400

    draw the motivation graphics

--- source/visualize/motivation.py
+++ source/visualize/motivation.py
@@ -3,47 +3,60 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from source.embedding import BgeBaseEmbedding
 
-sns.set_theme()
-palette = sns.color_palette()
-
 """
 Plot the original embedding.
 """
 
-# text = "Hello, World!"
-# embedding = BgeBaseEmbedding()
-# vector = embedding.forward(text).cpu().numpy()
-# vector = np.clip(vector, -0.14, 0.12)
+text = "Hello, World!"
+embedding = BgeBaseEmbedding()
+vector = embedding.forward(text).cpu().numpy()
+vector = np.clip(vector, -0.14, 0.12)
 
-# plt.axis("off")
-# plt.gca().spines["top"].set_visible(False)
-# plt.gca().spines["right"].set_visible(False)
-# plt.gca().spines["left"].set_visible(False)
-# plt.gca().spines["bottom"].set_visible(False)
-# plt.plot(vector[0], color=palette[0])
-# plt.savefig("saved.pdf", bbox_inches="tight", pad_inches=0)
-# plt.savefig("saved.svg", bbox_inches="tight", pad_inches=0)
-
-"""
-Plot the reconstructed embedding.
-"""
-
-# plt.axis("off")
-# plt.gca().spines["top"].set_visible(False)
-# plt.gca().spines["right"].set_visible(False)
-# plt.gca().spines["left"].set_visible(False)
-# plt.gca().spines["bottom"].set_visible(False)
-# plt.plot(vector[0], color=palette[1])
-# plt.savefig("saved.pdf", bbox_inches="tight", pad_inches=0)
-# plt.savefig("saved.svg", bbox_inches="tight", pad_inches=0)
+plt.axis("off")
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+plt.gca().spines["left"].set_visible(False)
+plt.gca().spines["bottom"].set_visible(False)
+plt.xlim(384 - 100, 384 + 100)
+plt.plot(vector[0], color="#00bcd4")
+plt.savefig("saved.svg", bbox_inches="tight", pad_inches=0, transparent=True)
 
 """
 Plot the sparse interpretable features.
 """
 
-sparse_features = np.random.randn(100)
+vector = np.concatenate(
+    [
+        np.repeat(6, 12),
+        np.repeat(8, 18),
+        np.repeat(10, 3),
+        np.repeat(12, 9),
+        np.repeat(15, 5),
+    ]
+)
 
-plt.hist(sparse_features, bins=30, color=palette[2])
-plt.savefig("saved.pdf", bbox_inches="tight", pad_inches=0)
-plt.savefig("saved.svg", bbox_inches="tight", pad_inches=0)
-plt.show()
+plt.axis("off")
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+plt.gca().spines["left"].set_visible(False)
+plt.gca().spines["bottom"].set_visible(False)
+plt.hist(vector, bins=20, color="#4caf50")
+plt.savefig("saved.svg", bbox_inches="tight", pad_inches=0, transparent=True)
+
+"""
+Plot the reconstructed embedding.
+"""
+
+text = "Goodbye, World!"
+embedding = BgeBaseEmbedding()
+vector = embedding.forward(text).cpu().numpy()
+vector = np.clip(vector, -0.14, 0.12)
+
+plt.axis("off")
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+plt.gca().spines["left"].set_visible(False)
+plt.gca().spines["bottom"].set_visible(False)
+plt.xlim(384 - 100, 384 + 100)
+plt.plot(vector[0], color="#ff9800")
+plt.savefig("saved.svg", bbox_inches="tight", pad_inches=0, transparent=True)
