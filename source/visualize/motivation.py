35944789ce6fedc574f3ffb0cca8b6fa7e5c95f9
haok1402 <haok1402@gmail.com>
2024-10-05 01:41:10 -0400

    completed with motivation

--- source/visualize/motivation.py
+++ source/visualize/motivation.py
@@ -1,5 +1,4 @@
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 from source.embedding import BgeBaseEmbedding
 
@@ -59,4 +58,30 @@ plt.gca().spines["left"].set_visible(False)
 plt.gca().spines["bottom"].set_visible(False)
 plt.xlim(384 - 100, 384 + 100)
 plt.plot(vector[0], color="#ff9800")
+plt.savefig("saved.svg", bbox_inches="tight", pad_inches=0, transparent=True)
+
+"""
+Plot the manipulated sparse features.
+"""
+
+vector = np.concatenate(
+    [
+        np.repeat(6, 12),
+        np.repeat(8, 18),
+        np.repeat(10, 35),
+        np.repeat(12, 9),
+        np.repeat(15, 50),
+    ]
+)
+
+plt.axis("off")
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+plt.gca().spines["left"].set_visible(False)
+plt.gca().spines["bottom"].set_visible(False)
+colors = ["#4caf50" for _ in range(20)]
+colors[8], colors[19] = "#f44336", "#f44336"
+_, _, patches = plt.hist(vector, bins=20)
+for c, p in zip(colors, patches):
+    plt.setp(p, "facecolor", c)
 plt.savefig("saved.svg", bbox_inches="tight", pad_inches=0, transparent=True)
